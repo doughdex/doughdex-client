4f1e3f4e9c10869ff2fb58c787fe6306c40787c2
Patrick Nilan <nilan.patrick@gmail.com>
2023-10-27 09:00:56 -0700

    Added landing page image to project folder for faster load

--- src/pages/ComingSoon.jsx
+++ src/pages/ComingSoon.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import brand from '../assets/img/brand-coming-soon.png';
+import pizzaGuy from '../assets/img/pizza-guy.jpeg';
 
 const ComingSoon = () => {
 
@@ -8,7 +9,7 @@ const ComingSoon = () => {
     <div className="left-landing">
       <img src={brand} alt="logo" />
     </div>
-    <div className="right-landing"></div>
+    <div className="right-landing" style={{backgroundImage: `url(${pizzaGuy})`}}></div>
   </section>
   );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
-        test: /\.png/,
+        test: /\.(png|jpg|jpeg)/,
         type: 'asset/resource',
       },
     ],
